199aee41900f6f1519f373baf47fe0c253846fde
Shrief Essam <shriefessam1999@gmail.com>
2022-11-17 20:33:20 +0200

    show the meal list on the screen

--- src/components/mealList/MealItem.jsx
+++ src/components/mealList/MealItem.jsx
@@ -1,12 +1,12 @@
-function MealItem(){
+function MealItem(props){
     return(
-        <div className="flex justify-between px-10 py-2">
+        <div className="flex justify-between border-b border-transparent pb-1 last:border-white">
             <div>
-                <p className="text-black font-extrabold tracking-tighter text-sm">Sushi</p>
-                <p className="text-black text-xs italic">Finest fish and veggies</p>
-                <p className="text-fireRed font-bold ">$22.99</p>
+                <p className="text-black font-extrabold tracking-tighter text-sm">{props.name}</p>
+                <p className="text-black text-xs italic">{props.description}</p>
+                <p className="text-fireRed font-bold ">${props.price}</p>
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1 justify-center">
                 <div className="flex gap-4 items-center">
                     <p className="text-black font-bold text-sm">Amount</p>
                     <input className="text-black border-transparent rounded-lg border w-9 h-5 text-sm pl-1" type="number" name="amount"/>
--- src/components/mealList/MealList.jsx
+++ src/components/mealList/MealList.jsx
@@ -1,10 +1,12 @@
 import MealItem from "./MealItem"
+import data from '../../data'
 
 function MealList(){
+    const meals = data.map(item=><MealItem key={item.id} {...item}/>)
     return(
         <div>
-            <div className='bg-white max-w-2xl m-auto rounded-2xl'>
-                <MealItem/>
+            <div className='bg-white max-w-2xl m-auto px-10 py-2 rounded-2xl flex flex-col gap-3'>
+                {meals}
             </div>
         </div>
         
