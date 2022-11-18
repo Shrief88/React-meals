8f74f7040d6d073c4d4fd7f0de89c37ba2a696df
Shrief Essam <shriefessam1999@gmail.com>
2022-11-18 13:59:28 +0200

    extract the data from data file

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import Header from './components/header/Header'
 import MealList from './components/mealList/MealList'
 import Main from './components/Main'
 import React, { useState } from 'react'
+import data from './data'
 
 
 export const OrderContext = React.createContext();
 
 function App() {
 
-  const [orders,setOrders] = useState([
-    {name : 'Sushi', amount : 0},
-    {name : 'Schnitzel', amount :0},
-    {name :'Barbecue Burger', amount:0},
-    {name : 'Green Bowl', amount:0}
-  ])
+  const[orders,setOrders] = useState(data.map(item=>{
+    return{
+      name :item.name,
+      price : item.price,
+      amount : 0
+    }
+   }))
 
   const updateOrders = (name,amount) =>{
       setOrders(prev=>prev.map(item=>item.name === name ? {...item,amount:amount}:item))
   }
-  
-  
   
   return (
     <div>
