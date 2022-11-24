1f4d88248257ddaed0dd9efef2a716bc12648124
Shrief Essam <shriefessam1999@gmail.com>
2022-11-24 19:50:09 +0200

    add animation for the cart  div

--- src/components/header/Cart.jsx
+++ src/components/header/Cart.jsx
@@ -1,13 +1,31 @@
 import { BsCart } from 'react-icons/bs';
 import { OrderContext } from '../../App';
-import { useContext } from 'react';
+import { useContext,useState,useEffect } from 'react';
+
 
 function Cart(){
     const ctx = useContext(OrderContext);
     const itemsCounter = ctx.orders.reduce((prev,cur)=>prev+Number(cur.amount),0)
+    const [addAnimation,setAddAnimation] = useState(false);
+
+    const animation = addAnimation ? 'animate-pump' : '';
+
+    useEffect(()=>{
+        if(itemsCounter === 0){
+            return;
+        }
+        setAddAnimation(true);
+        const timer =setTimeout(()=>{
+            setAddAnimation(false)
+        },300)
+
+        return()=>{
+            clearTimeout(timer);
+        }   
+    },[itemsCounter])
 
     return(
-        <div onClick={ctx.showModal} className=" bg-[#3D1505] rounded-2xl flex gap-3 items-center px-6 py-1 ">
+        <div onClick={ctx.showModal} className={`bg-[#3D1505] rounded-2xl flex gap-3 items-center px-6 py-1 ${animation}`}>
             <BsCart />
             <p className="font-bold text-sm tracking-tight">Your Cart</p>
             <div className='bg-red-800 py-0.5 px-2 rounded-lg'>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,20 @@ module.exports = {
   ],
   
   theme: {
-    extend: {}  
+    extend: {
+      keyframes: {
+        pump: {
+          '0% ,100%': { transform: 'scale(1)' },
+          '10%': { transform: 'scale(0.9)' },
+          '30%': { transform: 'scale(1.1)'},
+          '50%': { transform: 'scale(1.15)'},
+        }
+      },
+      animation:{
+        pump : 'pump 300ms ease-out'
+      } 
+    } 
+     
   },
   
 }
