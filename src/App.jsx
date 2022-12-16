0e46d8c1b367fad2ae45127d0410b822f318f9d4
Shrief Essam <shriefessam1999@gmail.com>
2022-12-16 12:42:30 +0200

    use api to fetch data form it

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,37 @@ import MealList from './components/mealList/MealList'
 import Main from './components/Main'
 import Modal from './components/modal/Modal'
 import React, { useState } from 'react'
-import data from './data'
+import { useEffect } from 'react'
 
 
 export const OrderContext = React.createContext();
 
 function App() {
+  const[meals,setMeals] = useState([]);
+  const[isLoading,setIsLoading] = useState(false);
+  const[orders,setOrders] = useState([]);
 
-  const[orders,setOrders] = useState(data.map(item=>{
-    return{
-      name :item.name,
-      price : item.price,
-      id:item.id,
-      amount : 0
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      setIsLoading(true);
+      const response = await fetch('https://reactmeals-755d6-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+      let loadedData = [];
+      for(const key in responseData){
+        loadedData.push({
+          id:key,
+          name:responseData[key].name,
+          price:responseData[key].price,
+          description:responseData[key].description
+        });
+      }
+      setMeals(loadedData);
+      setIsLoading(false);
     }
-  }))
-
-  const [modal,setModal] = useState(false);
-
+    fetchData();
+  },[])
+  
+ 
   const updateOrders = (id,amount) =>{
     setOrders(prev=>prev.map(item=>item.id === id ? {...item,amount:Number(item.amount+Number(amount))}:item))
   }
@@ -34,7 +47,7 @@ function App() {
     setOrders(prev=>prev.map(item=>item.id === id ? {...item,amount:Number(item.amount-1)}:item))
   }
 
-
+  const [modal,setModal] = useState(false);
 
   const showModal = ()=> setModal(prev=>!prev);
   
@@ -45,7 +58,7 @@ function App() {
         <Header/>
         <Main/>
         {modal && <Modal/>}
-        <MealList/>
+        <MealList meals={meals} isLoading={isLoading}/>
       </OrderContext.Provider>
       
     </div>
--- src/components/mealList/MealList.jsx
+++ src/components/mealList/MealList.jsx
@@ -1,12 +1,12 @@
 import MealItem from "./MealItem"
-import data from '../../data'
 
-function MealList(){
-    const meals = data.map(item=><MealItem key={item.id} {...item}/>)
+function MealList(props){
+    const meals = props.meals.map(item=><MealItem key={item.id} {...item}/>)
     return(
         <div>
             <div className='bg-white max-w-2xl m-auto px-10 py-2 mb-10 rounded-2xl flex flex-col gap-3'>
-                {meals}
+                {props.isLoading && <p className="text-black">Loading data...</p>}
+                {!props.isLoading && meals}
             </div>
         </div>
         
